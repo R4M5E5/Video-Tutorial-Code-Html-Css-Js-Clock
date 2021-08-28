a68c8df70a13dd9a555117f4a089da27a93dba02
Olli <58643307+R4M5E5@users.noreply.github.com>
2021-08-28 20:53:41 +0200

    Get clock hand dom nodes and add function for rotating dom nodes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+const secondHand = document.getElementById("second-hand");
+const minuteHand = document.getElementById("minute-hand");
+const hourHand = document.getElementById("hour-hand");
+
+function setRotationInTurns(domNode, turns) {
+    const transform = `rotate(${turns}turn)`;
+    const style = domNode.style;
+    style.webkitTransform = transform;
+    style.mozTransform = transform;
+    style.msTransform = transform;
+    style.oTransform = transform;
+    style.transform = transform;
+}
+
+setRotationInTurns(secondHand, 0.25);
+setRotationInTurns(minuteHand, 0.5);
+setRotationInTurns(hourHand, 0.75);
